a944967ee4cef29c52fe721b461b85fca4d3789a
Josh Varney <joshcainvarney@gmail.com>
2018-07-06 00:29:00 -0400

    Add files via upload

--- encrypt_hash/encrypt_hash.rb
+++ encrypt_hash/encrypt_hash.rb
@@ -1,101 +1,95 @@
-def encrypt_hash(message, shift)
-	# require 'date'
-	# shift = Date.today.to_s.split(//).last(2).join("").to_i
-	message_indexes = []
-	encrypted_message = []
+def char_hash
 	char1 = ('A'..'z').to_a - ['\\']
 	char2 = ('!'..'!').to_a
 	char3 = ('#'..'?').to_a
 	char_hash = char1 + char2 + char3
-	total_char = char_hash.count
 	# seed = 2
 	# char_hash = char_hash.shuffle(random: Random.new(seed))
 	char_hash = char_hash.map.with_index { |x, i| [i, x]}.to_h
+end
+def shift
+	# require 'date'
+	# shift = Date.today.to_s.split(//).last(2).join("").to_i
+	shift = 5
+end
+def shift_hash(shift)
+	shift_hash = char_hash
+	shift_hash = shift_hash.values.rotate(shift)
+	shift_hash = shift_hash.map.with_index { |x, i| [i, x]}.to_h
+end
+def encrypt_hash(message)
+	message_indexes = []
+	encrypted_message = []
+	char_hash = char_hash()
+	total_char = char_hash.count
 	message = message.split(//)
 	message.each { |y| y 
 		if y == " "
 			message_indexes.push(y)
 		else
 			char_hash.each { |i, x| x 
-			if y == x && i > total_char - 1 - shift
-				char_hash = char_hash.values.rotate(shift)
-				char_hash = char_hash.map.with_index { |x, i| [i, x]}.to_h
-				i = i - total_char + shift
-				message_indexes.push(i)
-				char_hash = char_hash.values.rotate(-shift)
-				char_hash = char_hash.map.with_index { |x, i| [i, x]}.to_h
-			elsif  y == x 
-				char_hash = char_hash.values.rotate(shift)
-				char_hash = char_hash.map.with_index { |x, i| [i, x]}.to_h
-				i = i + shift
-				message_indexes.push(i)
-				char_hash = char_hash.values.rotate(-shift)
-				char_hash = char_hash.map.with_index { |x, i| [i, x]}.to_h
-			end
+				if y == x && i > total_char - 1 - shift
+					char_hash = shift_hash(shift)
+					i = i - total_char + shift
+					message_indexes.push(i)
+					char_hash = char_hash()
+				elsif  y == x 
+					char_hash = shift_hash(shift)
+					i = i + shift
+					message_indexes.push(i)
+					char_hash = char_hash()				
+				end
 			}
 		end
-		}
+	}
 	message_indexes.each { |y| y
 		if y == " " 
 			encrypted_message.push(y)
 		else
-		char_hash.each { |i, x| i
-			if y == i
-				encrypted_message.push(x)
-			end
+			char_hash.each { |i, x| i
+				if y == i
+					encrypted_message.push(x)
+				end
 			}
 		end
-		}
+	}
 	encrypted_message = encrypted_message.join
 end
-def decrypt_hash(message, shift)
-	# require 'date'
-	# shift = Date.today.to_s.split(//).last(2).join("").to_i
+def decrypt_hash(message)
 	message_indexes = []
 	decrypted_message = []
-	char1 = ('A'..'z').to_a - ['\\']
-	char2 = ('!'..'!').to_a
-	char3 = ('#'..'?').to_a
-	char_hash = char1 + char2 + char3
+	char_hash = char_hash()
 	total_char = char_hash.count
-	# seed = 2
-	# char_hash = char_hash.shuffle(random: Random.new(seed))
-	char_hash = char_hash.map.with_index { |x, i| [i, x]}.to_h
 	message = message.split(//)
 	message.each { |y| y 
 		if y == " "
 			message_indexes.push(y)
 		else
 			char_hash.each { |i, x| x 
-			if y == x && i - shift < shift
-				char_hash = char_hash.values.rotate(-shift)
-				char_hash = char_hash.map.with_index { |x, i| [i, x]}.to_h
-				i = i + total_char - shift
-				message_indexes.push(i)
-				char_hash = char_hash.values.rotate(shift)
-				char_hash = char_hash.map.with_index { |x, i| [i, x]}.to_h
-			elsif  y == x 
-				char_hash = char_hash.values.rotate(-shift)
-				char_hash = char_hash.map.with_index { |x, i| [i, x]}.to_h
-				i = i - shift
-				message_indexes.push(i)
-				char_hash = char_hash.values.rotate(shift)
-				char_hash = char_hash.map.with_index { |x, i| [i, x]}.to_h
-			end
+				if y == x && i - shift < shift
+					char_hash = shift_hash(-shift)
+					i = i + total_char - shift
+					message_indexes.push(i)
+					char_hash = char_hash()
+				elsif  y == x 
+					char_hash = shift_hash(-shift)
+					i = i - shift
+					message_indexes.push(i)
+					char_hash = char_hash()
+				end
 			}
 		end
-		}
+	}
 	message_indexes.each { |y| y
 		if y == " " 
 			decrypted_message.push(y)
 		else
-		char_hash.each { |i, x| i
-			if y == i
-				decrypted_message.push(x)
-			end
+			char_hash.each { |i, x| i
+				if y == i
+					decrypted_message.push(x)
+				end
 			}
 		end
-		}
+	}
 	decrypted_message = decrypted_message.join
 end
-
--- encrypt_hash/encrypt_hash_test.rb
+++ encrypt_hash/encrypt_hash_test.rb
@@ -3,51 +3,51 @@ require_relative "encrypt_hash.rb"
 class Test_Encrypt_hash < Minitest::Test 
 	
 	def test_assert_that_we_can_change_letters1
-	 	assert_equal("Mjqqt", encrypt_hash("Hello", 5))
+	 	assert_equal("Mjqqt", encrypt_hash("Hello"))
 	end 
 	def test_assert_that_works_with_z2
-		assert_equal("`jgwf", encrypt_hash("Zebra", 5))
+		assert_equal("`jgwf", encrypt_hash("Zebra"))
 	end
 	def test_assert_that_works_with_a_to_e3
-		assert_equal("fiiji", encrypt_hash("added", 5))
+		assert_equal("fiiji", encrypt_hash("added"))
 	end
 	def test_assert_that_decrypting_works4
-		assert_equal("added", decrypt_hash("fiiji", 5))
+		assert_equal("added", decrypt_hash("fiiji"))
 	end
 	def test_assert_that_works_with_spaces5
-		assert_equal("Mjqqt `jgwf", encrypt_hash("Hello Zebra", 5))
+		assert_equal("Mjqqt `jgwf", encrypt_hash("Hello Zebra"))
 	end
 	def test_assert_that_decrypting_works_with_a_to_e6
-		assert_equal('[]^_`', decrypt_hash("abcde", 5))
+		assert_equal('[]^_`', decrypt_hash("abcde"))
 	end
 	def test_assert_that_decrypting_works_with_v_to_z7
-		assert_equal("qrstu", decrypt_hash("vwxyz", 5))
+		assert_equal("qrstu", decrypt_hash("vwxyz"))
 	end
 	def test_assert_that_what_happens_with_special_characters8
-		assert_equal("]mfy,x", encrypt_hash("What's", 5))
+		assert_equal("]mfy,x", encrypt_hash("What's"))
 	end
 	def test_assert_that_what_happens_with_numbers9
-		assert_equal("5>$%fg", encrypt_hash("09xyab", 5))
+		assert_equal("5>$%fg", encrypt_hash("09xyab"))
 	end
 	def test_assert_that_special_characters_change10
-		assert_equal("4ECDA", encrypt_hash("/?=>;", 5))
+		assert_equal("4ECDA", encrypt_hash("/?=>;"))
 	end
 	def test_abc_returns_fgh11
-		assert_equal("fgh", encrypt_hash("abc", 5))
+		assert_equal("fgh", encrypt_hash("abc"))
 	end
 	def test_that_this_works12
-		assert_equal("$%&", encrypt_hash("xyz", 5))
+		assert_equal("$%&", encrypt_hash("xyz"))
 	end
 	def test_that_that_works13
-		assert_equal("ABCDE", encrypt_hash(";<=>?", 5))
+		assert_equal("ABCDE", encrypt_hash(";<=>?"))
 	end	
 	def test_assert_that_special_characters_change_back14
-		assert_equal("/`^_]", decrypt_hash("4ecdb", 5))
+		assert_equal("/`^_]", decrypt_hash("4ecdb"))
 	end
 	def test_assert_that_works_with_spaces_decrypting15
-		assert_equal("Hello Zebra", decrypt_hash("Mjqqt `jgwf", 5))
+		assert_equal("Hello Zebra", decrypt_hash("Mjqqt `jgwf"))
 	end
 	def test_assert_that_decrypt_works_on_ABCDE16
-		assert_equal(";<=>?", decrypt_hash("ABCDE", 5))
+		assert_equal(";<=>?", decrypt_hash("ABCDE"))
 	end
 end
